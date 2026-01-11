a6678c4ebb1fbd423fcb0b60690fced739f41660
Quinton Loges <github@quintonloges.me>
2026-01-11 12:17:38 -0500

    Added QLinkedList<T> and coverage

---
+++ DataStructuresAndAlgorithms/DataStructures/QLinkedList.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms.DataStructures {
+  internal class QLinkedList<T> {
+    private QNode<T>? head;
+
+    /// <summary>
+    /// Pushes new data to the front of the list.
+    /// </summary>
+    /// <param name="data">Data to store.</param>
+    public void Push(T data) {
+      QNode<T> newNode = new QNode<T> {
+        Value = data,
+        Next = head
+      };
+      head = newNode;
+    }
+
+    /// <summary>
+    /// Searches the list for the specified data.
+    /// </summary>
+    /// <param name="data">Value of QNode to retrieve.</param>
+    /// <returns>QNode containing the value if found, null if not.</returns>
+    public QNode<T>? GetNode(T data) {
+      QNode<T>? cur = head;
+      while (cur != null) {
+        if (cur.Value!.Equals(data)) {
+          return cur;
+        }
+        cur = cur.Next;
+      }
+      return null;
+    }
+
+    /// <summary>
+    /// Find whether the given data exists in the list.
+    /// </summary>
+    /// <param name="data">Value to find.</param>
+    /// <returns>True if data exists, false if not</returns>
+    public bool Contains(T data) {
+      return GetNode(data) != null;
+    }
+
+    /// <summary>
+    /// Removes the first instance of the found value.
+    /// </summary>
+    /// <param name="data">Value to remove.</param>
+    /// <exception cref="KeyNotFoundException">Thrown if the value is not found in the list.</exception>
+    public void RemoveValue(T data) {
+      QNode<T>? prev = null;
+      QNode<T>? cur = head;
+      while (cur != null) {
+        if (cur.Value!.Equals(data)) {
+          break;
+        }
+        prev = cur;
+        cur = cur.Next;
+      }
+      if (cur == null) {
+        throw new KeyNotFoundException("The value specified in the argument could not be found.");
+      }
+      if (prev == null) {
+        head = null;
+        return;
+      }
+      prev.Next = cur.Next;
+    }
+
+    /// <summary>
+    /// Pops the value at the beginning of the List.
+    /// </summary>
+    /// <returns>QNode from the beginning of the list.</returns>
+    /// <exception cref="IndexOutOfRangeException">Thrown if the collection is empty.</exception>
+    public QNode<T> Pop() {
+      QNode<T>? cur = head;
+      if (head == null) {
+        throw new IndexOutOfRangeException("Cannot pop value out of empty list.");
+      }
+      head = head.Next;
+      return cur!;
+    }
+
+    /// <summary>
+    /// Returns the QNode at the beginning of the list.
+    /// </summary>
+    /// <returns>QNode at the beginning of the list.</returns>
+    public QNode<T>? Peek() {
+      return head;
+    }
+
+    public override string ToString() {
+      QNode<T>? cur = head;
+      string ret = "[";
+      while (cur != null) {
+        ret += cur.ToString();
+        if (cur.Next != null) {
+          ret += ", ";
+        }
+        cur = cur.Next;
+      }
+      ret += "]";
+      return ret;
+    }
+  }
+
+  internal class QNode<T> {
+    public required T Value;
+    public QNode<T>? Next;
+
+    public override string ToString() {
+      return Value!.ToString();
+    }
+  }
+}
---
+++ DataStructuresAndAlgorithms/Program.cs
@@ -0,0 +1,42 @@
+﻿using DataStructuresAndAlgorithms.DataStructures;
+
+namespace DataStructuresAndAlgorithms {
+  internal class Program {
+    static void Main(string[] args) {
+      // 100% coverage of QLinkedList
+      QLinkedList<string> testList = new QLinkedList<string>();
+      testList.Push("First");
+      testList.Push("Middle");
+      testList.Push("Last");
+
+      Console.WriteLine("Data has been insterted in the linked list.");
+      Console.WriteLine($"Data: {testList.ToString()}");
+
+      Console.WriteLine($"Contains value 'Last': {testList.Contains("Last")}");
+      Console.WriteLine($"Contains value 'Middle': {testList.Contains("Middle")}");
+      Console.WriteLine($"Contains value 'First': {testList.Contains("First")}");
+      Console.WriteLine($"Contains value 'Second': {testList.Contains("Second")}");
+
+      Console.WriteLine("Removing 'Middle'...");
+      testList.RemoveValue("Middle");
+      Console.WriteLine($"Data: {testList.ToString()}");
+      Console.WriteLine($"Contains value 'Middle': {testList.Contains("Middle")}");
+
+      Console.WriteLine("Finding node 'Last'...");
+      QNode<string>? last = testList.GetNode("Last");
+      Console.WriteLine($"Value of found node: {last?.Value ?? "Not found"}");
+
+      Console.WriteLine("Popping node...");
+      QNode<string>? popped = testList.Pop();
+      Console.WriteLine($"Value of popped node: {popped?.Value ?? "Not found"}");
+      Console.WriteLine($"Data: {testList.ToString()}");
+
+      Console.WriteLine("Peeking node...");
+      QNode<string>? peeked = testList.Peek();
+      Console.WriteLine($"Value of peeked node: {peeked?.Value ?? "Not found"}");
+      Console.WriteLine($"Data: {testList.ToString()}");
+
+      Console.ReadKey();
+    }
+  }
+}
